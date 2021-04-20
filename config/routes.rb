8f1361262695d3bfd3a5d755581e9967f42375a6
teramoto-chihiro <komorikiri1031@gmail.com>
2021-04-20 11:25:08 +0000

    html,css作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
 end
